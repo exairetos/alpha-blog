d50a7f2dee399e301fc13b108a6260e2fb718cae
Alexiou Konstantinos <alexiou_kon@yahoo.gr>
2020-04-07 16:19:45 +0300

    add new and create routes and controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,19 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+  end
+
+  def create
+    # render plain: params[:article]
+    @article =Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article.inspect
+    @article.save
+
+    #redirect
+    # /articles/:id(.:format)
+    # redirect_to article_path(@article)
+    # can be writen as
+    redirect_to @article
+  end
 end
