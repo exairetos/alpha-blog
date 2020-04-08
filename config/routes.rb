c62ad83f1ca8cc8bc418dc9b08c2ae498d082d54
Alexiou Konstantinos <alexiou_kon@yahoo.gr>
2020-04-08 16:13:14 +0300

    Add Layout links to all pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  # Adding the destroy route means we have reveal all auto created from the code below:
+  
+  resources :articles
 end
