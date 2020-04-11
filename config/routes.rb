f0125512417173e942cce1ef0f4ec0d2fc8c9b09
Alexiou Konstantinos <alexiou_kon@yahoo.gr>
2020-04-11 18:44:44 +0300

    Add new users signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update]
   # Adding the destroy route means we have reveal all auto created from the code below:
-  
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
