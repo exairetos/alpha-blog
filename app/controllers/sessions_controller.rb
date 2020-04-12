8b4d031b59a75a5bde9650bf7602ae85daccb36a
Alexiou Konstantinos <alexiou_kon@yahoo.gr>
2020-04-12 23:36:29 +0300

    Add login form and logout link

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,36 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    # byebug
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have succesfully logged in"
+      redirect_to user_path(user)
+    else
+      flash[:danger] = "There was something wrong with your login information"
+      render 'new'
+    end
+
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have succesfully logged out"
+    redirect_to root_path
+
+  end
+
+
+
+  private
+  def user_params
+    params.require(:session).permit(:email, :password)
+  end
+
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to the alpha  #{@user.username}, you have succesfully signed up."
-      redirect_to articles_path
+      redirect_to login_path
     else
       render 'new'
     end
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was succesfully updated"
-      redirect_to articles_path
+      redirect_to user_path
     else
       render 'edit'
     end
