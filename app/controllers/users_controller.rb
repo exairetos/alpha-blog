e5557639450b14652144c6b221dfee750761043f
Alexiou Konstantinos <alexiou_kon@yahoo.gr>
2020-04-13 17:00:10 +0300

    Add sign in uppon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the alpha  #{@user.username}, you have succesfully signed up."
-      redirect_to login_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
