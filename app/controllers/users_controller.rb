d887f706ed3eed18373972501da79ce0f1b389d4
Alexiou Konstantinos <alexiou_kon@yahoo.gr>
2020-04-13 15:00:12 +0300

    Add set_user in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit, :update, :show]
+
   def show
-    @user = User.find(params[:id])
     # @articles = @user.articles
     @pagy, @articles = pagy(@user.articles, items: 5)
   end
@@ -26,11 +27,9 @@ class UsersController < ApplicationController
 
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was succesfully updated"
       redirect_to user_path
@@ -42,6 +41,10 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 
