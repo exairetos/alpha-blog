c3e263ad85fc7b9a01881af8363ee6f3f74b9438
Alexiou Konstantinos <alexiou_kon@yahoo.gr>
2020-04-13 16:34:23 +0300

    Restrict actions from UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
     # render plain: params[:article]
     # whitelist the params we need
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     # render plain: @article.inspect
     if @article.save
       flash[:notice] = "Article was created succesfully."
