c3faadfd78fc2f75b9123a2cdabdb89dc74941d2
Alexiou Konstantinos <alexiou_kon@yahoo.gr>
2020-04-14 22:42:56 +0300

    Pass tests and refine navigation

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,21 +4,31 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
   end
 
-  test 'should get categories index' do
+  test 'should get index' do
     get categories_path
     assert_response :success
   end
 
-  test 'should get categories new' do
+  test 'should get new' do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_response :success
   end
 
-  test 'should get categories show' do
+  test 'should get show' do
 
     get category_path(@category)
     assert_response :success
   end
+
+  test "should redirect create when admin not looged in" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params:{category:{name: "sports"}}
+    end
+    assert_redirected_to categories_path
+  end
+
 end
--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,19 +2,25 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
-  test "get new category form and create category" do
+  def setup
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+  end
 
+  test "get new category form and create category" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
       post categories_path, params:{category:{name: "sports"}}
-      follow_redirect!
+
     end
+    follow_redirect!
     assert_template 'categories/index'
     assert_match "sports", response.body
   end
 
   test "invalid category submission results in failure" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
