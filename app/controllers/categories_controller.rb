ecdfc0c96c43567243e9747a368e16932c82cf96
Alexiou Konstantinos <alexiou_kon@yahoo.gr>
2020-04-14 18:45:49 +0300

    Correct messages in new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,7 +12,7 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
-      flash[:succes] = "Category was created succesfully"
+      flash[:success] = "Category was created succesfully"
       redirect_to categories_path
     else
       render 'new'
